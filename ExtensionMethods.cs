5e2661b974318641d2dbbeb2e0ca94b4140cb729
svCcare <maciej.pawlak94@gmail.com>
2023-11-29 15:09:43 +0100

    Add project files.

---
+++ ExtensionMethods.cs
@@ -0,0 +1,10 @@
+﻿namespace SharedTools
+{
+    public static class ExtensionMethods
+    {
+        public static IEnumerable<(T item, int index)> WithIndex<T>(this IEnumerable<T> source)
+        {
+            return source.Select((item, index) => (item, index));
+        }
+    }
+}
---
+++ FileReader.cs
@@ -0,0 +1,31 @@
+﻿namespace SharedTools
+{
+    public class FileReader
+    {
+        public string Text { get; private set; } = "";
+        public string[] TextLines { get; private set; } = new string[0];
+
+        public FileReader(string path, ReadOption readOption)
+        {
+            switch (readOption)
+            {
+                case ReadOption.All:
+                    Text = File.ReadAllText(path);
+                    break;
+                case ReadOption.Lines:
+                    TextLines = File.ReadAllLines(path);
+                    break;
+                default:
+                    Text = File.ReadAllText(path);
+                    break;
+            }
+
+        }
+    }
+
+    public enum ReadOption
+    {
+        All,
+        Lines
+    }
+}
