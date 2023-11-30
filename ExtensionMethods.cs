5fd2971b622631078eb89f883ea76ffa248d5f6f
svCcare <maciej.pawlak94@gmail.com>
2023-11-30 21:09:56 +0100

    Added two extension methods

--- ExtensionMethods.cs
+++ ExtensionMethods.cs
@@ -1,10 +1,27 @@
-﻿namespace SharedTools
+﻿using System.Text.RegularExpressions;
+
+namespace SharedTools
 {
     public static class ExtensionMethods
     {
+        private static readonly Regex sWhitespace = new Regex(@"\s+");
+
         public static IEnumerable<(T item, int index)> WithIndex<T>(this IEnumerable<T> source)
         {
             return source.Select((item, index) => (item, index));
+        }
+
+        public static string ReplaceWhitespace(this string input, string replacement = "")
+        {
+            return sWhitespace.Replace(input, replacement);
+        }
+
+        public static T PopMax<T>(this IList<T> source) where T : struct, IComparable, IFormattable, IConvertible, IComparable<T>, IEquatable<T>
+        {
+            var max = source.Max();
+            source.Remove(max);
+
+            return max;
         }
     }
 }
